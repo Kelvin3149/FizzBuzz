c559023b1a69de0f45c480995da335ed33a3e03e
Kelvin3149 <kelvin.luu40@gmail.com>
2025-07-01 16:26:57 +0200

    Initial commit

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,17 @@
-# Prints number 1 to 100 
-
+"""In a language of your choice write a program that prints the numbers from 1 to 100.
+But for multiples of three print "Fizz" instead of the number and for the multiples of five print
+"Buzz".
+For numbers which are multiples of both three and five print "FizzBuzz"."""
 
 def fizzbuzz(start, end):
     for i in range(start, end + 1):
-        # returns FizzBuzz 
+        # Returns FizzBuzz if number is divisible by 3 and 5
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
+        # Returns Fizz if number is divisible by 3
         elif i % 3 == 0:
             print("Fizz")
+        # Returns Buzz if number is divisible by 5
         elif i % 5 == 0:
             print("Buzz")
         else:
