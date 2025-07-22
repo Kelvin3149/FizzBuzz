aea5ef7fe42f2075229ff8ff4fb8796bc9fba0ac
Kelvin3149 <kelvin.luu40@gmail.com>
2025-07-22 13:22:37 +0200

    Change output from "Buzz" to "Buss"

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,19 +1,21 @@
-"""In a language of your choice write a program that prints the numbers from 1 to 100.
-But for multiples of three print "Fizz" instead of the number and for the multiples of five print
-"Buzz".
-For numbers which are multiples of both three and five print "FizzBuzz"."""
+DIVISIBLE_BY_3_OUTPUT = "Fizz"
+DIVISIBLE_BY_5_OUTPUT = "Buss"
+DIVISIBLE_BY_BOTH_OUTPUT = DIVISIBLE_BY_3_OUTPUT+DIVISIBLE_BY_5_OUTPUT
 
 def fizzbuzz(start, end):
+    """
+    Prints numbers from start to end.
+    Prints DIVISIBLE_BY_3_OUTPUT for multiples of 3,
+    DIVISIBLE_BY_5_OUTPUT for multiples of 5,
+    DIVISIBLE_BY_BOTH_OUTPUT for multiples of both.
+    """
     for i in range(start, end + 1):
-        # Returns FizzBuzz if number is divisible by 3 and 5
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        # Returns Fizz if number is divisible by 3
+            print(DIVISIBLE_BY_BOTH_OUTPUT)
         elif i % 3 == 0:
-            print("Fizz")
-        # Returns Buzz if number is divisible by 5
+            print(DIVISIBLE_BY_3_OUTPUT)
         elif i % 5 == 0:
-            print("Buzz")
+            print(DIVISIBLE_BY_5_OUTPUT)
         else:
             print(i)
 
